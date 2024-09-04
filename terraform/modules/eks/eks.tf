69a17dc90f863c58c5af7a148682873f7d10ec56
viki-vik <77933123+viki-vik@users.noreply.github.com>
2024-09-04 23:50:48 +0300

    Update eks.tf

--- terraform/modules/eks/eks.tf
+++ terraform/modules/eks/eks.tf
@@ -1,57 +1,39 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "20.18.0"
 
   cluster_name    = "devops2-eks-cluster"
   cluster_version = "1.30"
 
-  cluster_endpoint_public_access  = true
-
-  cluster_addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
-  }
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = false  # Set to true if you need private endpoint access
 
   vpc_id                   = "vpc-02c42497a5d880aa4"
   subnet_ids               = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
   control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
 
   # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-  }
-
-  eks_managed_node_groups = {
+  node_groups = {
     example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.xlarge"]
-
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
+      desired_capacity = 2
+      max_capacity     = 10
+      min_capacity     = 2
+      instance_types   = ["m5.xlarge"]
+      ami_type         = "AL2023_x86_64_STANDARD"  # Specify the AMI type for EKS managed node groups
     }
   }
 
-  # Cluster access entry
-  # To add the current caller identity as an administrator
+  # Access configuration
   enable_cluster_creator_admin_permissions = true
 
+  # IAM roles and policies for cluster access
   access_entries = {
-    # One access entry with a policy associated
     example = {
-      kubernetes_groups = []
       principal_arn     = "arn:aws:iam::123456789012:role/something"
-
+      kubernetes_groups = []
       policy_associations = {
         example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-          access_scope = {
-            namespaces = ["default"]
-            type       = "namespace"
-          }
+          policy_arn = "arn:aws:iam::aws:policy/AmazonEKSViewOnlyAccess"
         }
       }
     }
@@ -61,4 +43,5 @@ module "eks" {
     Environment = "dev"
     Terraform   = "true"
   }
+
 }
