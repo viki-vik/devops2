7e8f3433cac337ccacd06e43235659892f9bd333
viki-vik <77933123+viki-vik@users.noreply.github.com>
2024-09-04 23:21:35 +0300

    Update create_ec2.py

--- app/create_ec2.py
+++ app/create_ec2.py
@@ -1,5 +1,6 @@
 import boto3
 
+# Function to create an EC2 instance with a specific AMI instance type, and subnet
 def create_ec2_instance(ami_id, instance_type, subnet_id):
     ec2 = boto3.resource('ec2')
     instance = ec2.create_instances(
@@ -12,7 +13,11 @@ def create_ec2_instance(ami_id, instance_type, subnet_id):
     print(f"Created EC2 instance with ID: {instance[0].id}")
 
 if __name__ == "__main__":
+    # AMI ID for the instance to be launched
     ami_id = "ami-02c21308fed24a8ab"
+    # Instance type to be launched
     instance_type = "t3.micro"
+    # Subnet ID where the instance will be placed
     subnet_id = "subnet-0a40e7beb104e25cb"
+    # Call the function to create the EC2 instance with the specified parameters
     create_ec2_instance(ami_id, instance_type, subnet_id)
